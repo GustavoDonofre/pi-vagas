0cc6eb0c112bcd9a00128baa1196406bbf0a9768
Gustavo D Onofre QA Analyst <Gustavo.donofre@gmail.com>
2026-03-06 20:13:02 -0300

    Update page.js

--- app/main/vagas/page.js
+++ app/main/vagas/page.js
@@ -10,8 +10,8 @@ export default function Vagas() {
   const [modo_trabalho, SetModo_trabalho] = useState()
   const [periodo, SetPerido] = useState()
 
-  function cadastrar (e) {
-    e.preventDefault ()
+  function cadastrar(e) {
+    e.preventDefault()
 
     const vaga = {
       empresa: "empresa",
@@ -27,10 +27,6 @@ export default function Vagas() {
   return (
 
     <div>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Gerenciador de Vagas</title>
-
       <div className="container d-flex justify-content-center align-items-center min-vh-100">
         <div className="card shadow-lg p-4 col-md-6">
 
@@ -41,53 +37,53 @@ export default function Vagas() {
             <div className="mb-3">
               <label className="form-label">Empresa</label>
               <input type="text" className="form-control" placeholder="Nome da empresa"
-               onChange={(e) =>SetEmpresa (e.target.value)}/>
+                onChange={(e) => SetEmpresa(e.target.value)} />
             </div>
 
             <div className="mb-3">
               <label className="form-label">Área de Atuação</label>
-             <textarea className="form-control" rows="3"onChange={(e) => Setarea(e.target.value)}></textarea>
+              <textarea className="form-control" rows="3" onChange={(e) => Setarea(e.target.value)}></textarea>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Descrição da Vaga</label>
-              <textarea className="form-control" rows="3" onChange={(e) =>SetDescricao (e.target.value)}></textarea>
+              <textarea className="form-control" rows="3" onChange={(e) => SetDescricao(e.target.value)}></textarea>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Salário</label>
               <div className="input-group">
                 <span className="input-group-text">R$</span>
-                <input type="number" className="form-control" placeholder="0,00" onChange={(e) =>SetSalario (e.target.value)}/>
+                <input type="number" className="form-control" placeholder="0,00" onChange={(e) => SetSalario(e.target.value)} />
               </div>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Tipo de Vaga</label>
-              <select className="form-select" onChange={(e) =>SetTipo_vaga (e.target.value)}>
+              <select className="form-select" onChange={(e) => SetTipo_vaga(e.target.value)}>
                 <option select disabled>Selecione</option>
-                <option value= "efetiva" >Efetiva</option>
-                <option value= "freelancer" >Freelancer</option>
+                <option value="efetiva" >Efetiva</option>
+                <option value="freelancer" >Freelancer</option>
               </select>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Modo de Trabalho</label>
-              <select className="form-select" onChange={(e) =>SetModo_trabalho (e.target.value)}>
+              <select className="form-select" onChange={(e) => SetModo_trabalho(e.target.value)}>
                 <option select disabled>Selecione</option>
-                <option value= "remoto">Remoto</option>
-                <option value= "hibrido">Híbrido</option>
-                <option value= "presencial">Presencial</option>
+                <option value="remoto">Remoto</option>
+                <option value="hibrido">Híbrido</option>
+                <option value="presencial">Presencial</option>
               </select>
             </div>
 
             <div className="mb-4">
               <label className="form-label">Período</label>
-              <select className="form-select" onChange={(e) =>SetPerido (e.target.value)}>
+              <select className="form-select" onChange={(e) => SetPerido(e.target.value)}>
                 <option select disabled>Selecione</option>
-                <option value= "matutino">Matutino</option>
-                <option value= "vespertino">Vespertino</option>
-                <option value= "noturno">Noturno</option>
+                <option value="matutino">Matutino</option>
+                <option value="vespertino">Vespertino</option>
+                <option value="noturno">Noturno</option>
               </select>
             </div>
 
