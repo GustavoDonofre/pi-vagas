4142a0573930b96fe893af8bc9bd8e46a9d9a0c3
giselleoliveira2102-design <giselleoliveira2102@gmail.com>
2026-03-13 21:57:29 -0300

    aula importação de dados

--- app/main/vagas/page.js
+++ app/main/vagas/page.js
@@ -1,28 +1,57 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://qrcmtnxakmuwbunyoooc.supabase.co', 'sb_publishable_kD9z8OLZIlbh3yry6yNMDQ_LTAi81op')
 
 export default function Vagas() {
-  const [empresa, SetEmpresa] = useState("")
-  const [atuacao, SetAtucao] = useState("")
-  const [descricao, SetDescricao] = useState("")
-  const [salario, SetSalario] = useState()
-  const [tipo_vaga, SetTipo_vaga] = useState()
-  const [modo_trabalho, SetModo_trabalho] = useState()
-  const [periodo, SetPerido] = useState()
 
-  function cadastrar(e) {
+
+
+  const [empresa, alteraEmpresa] = useState("")
+  const [area, alteraArea] = useState("")
+  const [descricao, alteraDescricao] = useState("")
+  const [salario, alteraSalario] = useState("")
+  const [efetivo, alteraEfetivo] = useState("")
+  const [presencial, alteraPresencial] = useState("")
+  const [turno, alteraTurno] = useState("")
+
+  const [vagas, alteraVagas] = useState ([])
+
+
+  async function salvar (e) {
     e.preventDefault()
 
     const vaga = {
-      empresa: "empresa",
-      descricao: "descricao",
-      salario: " ",
-      vaga: "vaga",
-      modo: "modo",
-      periodo: "periodo"
+      id_empresa: empresa,
+      area: area,
+      descricao: descricao,
+      salario: salario,
+      efetivo: efetivo,
+      presencial: presencial,
+      turno: turno
     };
-    console.log(vaga)
+
+    const {error} = await supabase
+      .from ('cadastrovagas')
+      .insert ( vaga)
+    console.log(error)
+
+    if (error == null) {
+                alert ("vaga cadastrada com sucesso!")
+                alteraEmpresa ("")
+                alteraArea ("")
+                alteraDescricao ("")
+                alteraSalario ("")
+                alteraEfetivo ("")
+                alteraPresencial("")
+                alteraTurno ("")
+                //location.reload()
+            } else {
+                alert ("Dados inválidos, verifique os campos e tente novamente...")
+            }
   }
+
+
 
   return (
 
@@ -32,36 +61,36 @@ export default function Vagas() {
 
           <h1 className="text-center mb-4">Cadastro de Vagas</h1>
 
-          <form onSubmit={cadastrar}>
+          <form onSubmit={salvar}>
 
             <div className="mb-3">
               <label className="form-label">Empresa</label>
-              <input type="text" className="form-control" placeholder="Nome da empresa"
-                onChange={(e) => SetEmpresa(e.target.value)} />
+              <input value= {empresa} type="text" className="form-control" placeholder="Nome da empresa"
+                onChange={(e) => alteraEmpresa(e.target.value)} />
             </div>
 
             <div className="mb-3">
               <label className="form-label">Área de Atuação</label>
-              <textarea className="form-control" rows="3" onChange={(e) => Setarea(e.target.value)}></textarea>
+              <textarea value= {area} className="form-control" rows="3" onChange={(e) => alteraArea(e.target.value)}></textarea>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Descrição da Vaga</label>
-              <textarea className="form-control" rows="3" onChange={(e) => SetDescricao(e.target.value)}></textarea>
+              <textarea value= {descricao} className="form-control" rows="3" onChange={(e) => alteraDescricao(e.target.value)}></textarea>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Salário</label>
               <div className="input-group">
                 <span className="input-group-text">R$</span>
-                <input type="number" className="form-control" placeholder="0,00" onChange={(e) => SetSalario(e.target.value)} />
+                <input value= {salario} type="number" className="form-control" onChange={(e) => alteraSalario(e.target.value)} />
               </div>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Tipo de Vaga</label>
-              <select className="form-select" onChange={(e) => SetTipo_vaga(e.target.value)}>
-                <option select disabled>Selecione</option>
+              <select value= {efetivo} className="form-select" onChange={(e) => alteraEfetivo(e.target.value)}>
+                <option disabled value="">Selecione</option>
                 <option value="efetiva" >Efetiva</option>
                 <option value="freelancer" >Freelancer</option>
               </select>
@@ -69,18 +98,17 @@ export default function Vagas() {
 
             <div className="mb-3">
               <label className="form-label">Modo de Trabalho</label>
-              <select className="form-select" onChange={(e) => SetModo_trabalho(e.target.value)}>
-                <option select disabled>Selecione</option>
+              <select value= {presencial} className="form-select" onChange={(e) => alteraPresencial(e.target.value)}>
+                <option disabled value="">Selecione</option>
                 <option value="remoto">Remoto</option>
-                <option value="hibrido">Híbrido</option>
                 <option value="presencial">Presencial</option>
               </select>
             </div>
 
             <div className="mb-4">
-              <label className="form-label">Período</label>
-              <select className="form-select" onChange={(e) => SetPerido(e.target.value)}>
-                <option select disabled>Selecione</option>
+              <label className="form-label">Turno</label>
+              <select value= {turno} className="form-select" onChange={(e) => alteraTurno(e.target.value)}>
+                <option disabled value="">Selecione</option>
                 <option value="matutino">Matutino</option>
                 <option value="vespertino">Vespertino</option>
                 <option value="noturno">Noturno</option>
@@ -90,6 +118,21 @@ export default function Vagas() {
             <button type="submit" className="btn btn-warning">Cadastrar Vaga</button>
 
           </form>
+
+          <div>
+            <ul>
+                {
+                    vagas.length == 0 ?
+                        <p></p>
+                    :    
+                        vagas.map(
+                            item => <li> Empresa: {item.empresa} Area: {item.area} Descrição: {item.descricao} Salario: {item.salario} Tipo: {item.efetivo} Modo: {item.presencial} Turno: {item.turno}</li>
+                            
+                           
+                    )
+                }
+            </ul>
+          </div>
 
         </div>
       </div>
